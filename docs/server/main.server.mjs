96a523f56c3d728711df0a9f615f67102f94b63e
Your Name <you@example.com>
2024-11-21 10:20:41 +0530

    updated

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XLQIZRCC.mjs";import"./chunk-OGXT6IEW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SFNWJAH3.mjs";import"./chunk-7ASC72AO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';  // Bind email input
+  password: string = '';  // Bind password input
 
+  constructor() {}
+
+  // Method to handle form submission
+  onSubmit(): void {
+    if (this.email && this.password) {
+      console.log('Form Submitted!', { email: this.email, password: this.password });
+      // Logic to handle login request goes here, like sending data to an API.
+    } else {
+      console.log('Please fill out both email and password fields');
+    }
+  }
 }
