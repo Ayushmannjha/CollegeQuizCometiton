edf9ca29db4beefaf15b196881cc71401540a7ba
Your Name <you@example.com>
2024-11-26 21:15:08 +0530

    update docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TQHAV4M3.mjs";import"./chunk-OE24I2NK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-TBLF4I5V.mjs";import"./chunk-NPPIRZRL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
